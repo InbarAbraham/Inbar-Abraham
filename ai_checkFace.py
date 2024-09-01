f91bf11aa0aefb74040f9dd20cab83805b3baca3
InbarAbraham <152271955+InbarAbraham@users.noreply.github.com>
2024-09-01 14:27:44 +0300

    Create ai_checkFace.py
    Create ai_checkFace.py - python code

---
+++ ai_checkFace.py
@@ -0,0 +1,10 @@
+from deepface import DeepFace
+
+# ניתוח תמונה לדוגמה
+try:
+    # נשתמש בתמונה לדוגמה מהספרייה
+    obj = DeepFace.analyze(img_path = "https://raw.githubusercontent.com/serengil/deepface/master/tests/dataset/img1.jpg", actions = ['age', 'gender', 'race', 'emotion'])
+    print("הניתוח הצליח! הנה התוצאה:")
+    print(obj)
+except Exception as e:
+    print(f"שגיאה התרחשה: {e}")
